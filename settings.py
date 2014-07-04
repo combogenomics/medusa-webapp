42e30f89fc75b355f21e62c186890e1ce1d6eb5e
Marco Galardini <marcog@mario.dbe.unifi.it>
2014-07-04 12:17:37 +0200

    Upload limit up to one Gb

--- settings.py
+++ settings.py
@@ -5,4 +5,4 @@ SECRET_KEY='development key'
 CELERY_BROKER_URL='redis://localhost:6379'
 CELERY_RESULT_BACKEND='redis://localhost:6379'
 UPLOAD_FOLDER = 'uploads'
-MAX_CONTENT_LENGTH = 100 * 1024 * 1024
+MAX_CONTENT_LENGTH = 1024 * 1024 * 1024
